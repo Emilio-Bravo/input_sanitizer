1a605620445d6fd6cfd2976856b8c341bc2421c0
Emilio Bravo <78387018+Emilio-Bravo@users.noreply.github.com>
2021-04-30 11:47:04 -0500

    Update inputSanitizer.php
    New features:
    
    * Change replacement by modifiyng replacement property
    
    *New function sanitizeURL

--- inputSanitizer.php
+++ inputSanitizer.php
@@ -4,6 +4,9 @@ namespace sanitizer;
 
 class inputSanitizer
 {
+    
+    public static $replacement = "";
+    
     public static function sanitize($input)
     {
         return strip_tags(trim($input));
@@ -18,46 +21,50 @@ class inputSanitizer
     }
     public static function sanitizeAllSpaces($input)
     {
-         return inputSanitizer::sanitizeSpaces(preg_replace('/\s/', "", $input));
+         return inputSanitizer::sanitizeSpaces(preg_replace('/\s/', self::$replacement, $input));
     }
     public static function sanitizeNumbers($input)
     {
-        return inputSanitizer::sanitizeSpaces(preg_replace('/[0-9]/', "", $input));
+        return inputSanitizer::sanitizeSpaces(preg_replace('/[0-9]/', self::$replacement, $input));
     }
     public static function sanitizeWord($coincidence, $input)
     {
-        return inputSanitizer::sanitizeSpaces(preg_replace('/' . $coincidence . '/i', "", $input));
+        return inputSanitizer::sanitizeSpaces(preg_replace('/' . $coincidence . '/i', self::$replacement, $input));
     }
     public static function sanitizeLetters($input)
     {
-        return inputSanitizer::sanitizeSpaces(preg_replace('/[a-zA-Z]/',"",$input));
+        return inputSanitizer::sanitizeSpaces(preg_replace('/[a-zA-Z]/', self::$replacement,$input));
     }
     public static function sanitizeCharacters($input)
     {
-        return inputSanitizer::sanitizeSpaces(preg_replace('/\D/',"",$input));
+        return inputSanitizer::sanitizeSpaces(preg_replace('/\D/', self::$replacement,$input));
     }
     public static function sanitizeDigits($input)
     {
-        return inputSanitizer::sanitizeSpaces(preg_replace('/\d/',"",$input));
+        return inputSanitizer::sanitizeSpaces(preg_replace('/\d/', self::$replacement,$input));
     }
     public static function sanitizeImpurities($input)
     {
-        return inputSanitizer::sanitizeSpaces(preg_replace('/\W/',"",$input));
+        return inputSanitizer::sanitizeSpaces(preg_replace('/\W/', self::$replacement,$input));
     }
     public static function sanitizeUpperCase($input)
     {
-        return inputSanitizer::sanitizeSpaces(preg_replace("/[A-Z]/","",$input));
+        return inputSanitizer::sanitizeSpaces(preg_replace("/[A-Z]/", self::$replacement,$input));
     }
     public static function sanitizeLowerCase($input)
     {
-        return inputSanitizer::sanitizeSpaces(preg_replace("/[a-z]/","",$input));
+        return inputSanitizer::sanitizeSpaces(preg_replace("/[a-z]/", self::$replacement,$input));
     }
     public static function sanitizeXSS($input)
     {
-        return inputSanitizer::sanitizeSpaces(preg_replace("/^[on]+[a-z]+/m","",$input));
+        return inputSanitizer::sanitizeSpaces(preg_replace("/^[on]+[a-z]+/m", self::$replacement,$input));
     }
     public static function sanitizeEmail($input)
     {
-        return inputSanitizer::sanitizeSpaces(preg_replace("/\b[A-Za-z0-9._%@+-]+@[a-zA-z0-9]+\.[a-zA-Z]{2,6}\b/","",$input));
+        return inputSanitizer::sanitizeSpaces(preg_replace("/\b[A-Za-z0-9._%@+-]+@[a-zA-z0-9]+\.[a-zA-Z]{2,6}\b/", self::$replacement,$input));
+    }
+    public static function sanitizeURL($input)
+    {
+        return inputSanitizer::sanitizeSpaces(preg_replace("/^(http|https)+://+([\w\S\d])+\.([\w\d])+?([a-z])+/", self::$replacement,$input));
     }
 }
